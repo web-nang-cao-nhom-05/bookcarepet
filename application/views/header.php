feae006b45f63422c02e62a20b3773041d4a1780
kimlienkmhd <64739723+kimlienkmhd@users.noreply.github.com>
2020-05-04 22:02:42 +0700

    Add files via upload

---
+++ application/views/header.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>BookCarePet</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Amatic+SC:400,700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="<?=base_url();?>css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="<?=base_url();?>css/animate.css">
+    
+    <link rel="stylesheet" href="<?=base_url();?>css/owl.carousel.min.css">
+    <link rel="stylesheet" href="<?=base_url();?>css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="<?=base_url();?>css/magnific-popup.css">
+
+    <link rel="stylesheet" href="<?=base_url();?>css/aos.css">
+
+    <link rel="stylesheet" href="<?=base_url();?>css/ionicons.min.css">
+
+    <link rel="stylesheet" href="<?=base_url();?>css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="<?=base_url();?>css/jquery.timepicker.css">
+
+    
+    <link rel="stylesheet" href="<?=base_url();?>css/flaticon.css">
+    <link rel="stylesheet" href="<?=base_url();?>css/icomoon.css">
+    <link rel="stylesheet" href="<?=base_url();?>css/style.css">
+  </head>
+  <body class="goto-here">
+		<div class="py-1 bg-primary">
+    	<div class="container">
+    		<div class="row no-gutters d-flex align-items-start align-items-center px-md-0">
+	    		<div class="col-lg-12 d-block">
+		    		<div class="row d-flex">
+		    			<div class="col-md pr-4 d-flex topper align-items-center">
+					    	<div class="icon mr-2 d-flex justify-content-center align-items-center"><span class="icon-phone2"></span></div>
+						    <span class="text">+ 1235 2355 98</span>
+					    </div>
+					    <div class="col-md pr-4 d-flex topper align-items-center">
+					    	<div class="icon mr-2 d-flex justify-content-center align-items-center"><span class="icon-paper-plane"></span></div>
+						    <span class="text">bookcarepet@gmail.com</span>
+					    </div>
+					    <div class="col-md-5 pr-4 d-flex topper align-items-center text-lg-right">
+						    <span class="text">Đặt lịch khám trực tuyến 24/7 tại các phòng khám uy tín</span>
+					    </div>
+				    </div>
+			    </div>
+		    </div>
+		  </div>
+    </div>
+    <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+	    <div class="container">
+	      <a class="navbar-brand" href="index.html">BookCarePet</a>
+	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="oi oi-menu"></span> Menu
+	      </button>
+
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav ml-auto">
+	          <li class="nav-item"><a href="<?=base_url();?>trang_chu.html" class="nav-link">Trang Chủ</a></li>
+	          <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dịch Vụ</a>
+              <div class="dropdown-menu" aria-labelledby="dropdown04">
+              	<a class="dropdown-item" href="<?=base_url();?>dich_vu_cung_cap_do_cho_thu_cung.html">Dịch vụ cung cấp đồ cho thú cưng</a>
+              	<a class="dropdown-item" href="<?=base_url();?>dich_vu_cham_soc_kham_chua_cho_thu_cung.html">Dịch vụ chăm sóc, khám chữa cho thú cưng</a>
+              </div>
+            </li>
+	          <li class="nav-item active"><a href="<?=base_url();?>tin_tuc.html" class="nav-link">Tin Tức</a></li>
+	          <li class="nav-item"><a href="<?=base_url();?>phan_hoi.html" class="nav-link">Liên Hệ</a></li>
+	         
+
+	        </ul>
+	      </div>
+	    </div>
+	  </nav>
+    <!-- END nav -->
+
+    
+</body>
+
+</html>
