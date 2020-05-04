87a4815721d6d074d520c38ecffbdfb60aecd1ef
nguyenmai26634 <63508090+nguyenmai26634@users.noreply.github.com>
2020-05-05 01:35:41 +0700

    Update footer.php

--- application/views/footer.php
+++ application/views/footer.php
@@ -19,16 +19,10 @@
 		<section class="ftco-section ftco-no-pt ftco-no-pb py-5 bg-light">
       <div class="container py-4">
         <div class="row d-flex justify-content-center py-5">
-          <div class="col-md-6">
-          	<h2 style="font-size: 22px;" class="mb-0">Đăng ký để nhận email thông tin mới nhất về các phòng khám, dịch vụ và sản phẩm ưu đãi cho thú cưng</h2>
-          	
-          </div>
+          
           <div class="col-md-6 d-flex align-items-center">
             <form action="#" class="subscribe-form">
-              <div class="form-group d-flex">
-                <input type="text" class="form-control" placeholder="Nhập địa chỉ email">
-                <input type="submit" value="Đăng ký" class="submit px-3">
-              </div>
+              
             </form>
           </div>
         </div>
