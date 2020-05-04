4689c814f1654dbaddf7a4368df8423d941fc949
minhminh974 <47655575+minhminh974@users.noreply.github.com>
2020-05-05 03:26:35 +0700

    Add files via upload

---
+++ application/views/v_dich_vu_chi_tiet.php
@@ -0,0 +1,314 @@
+
+        <!-- END nav -->
+
+    <div class="hero-wrap hero-bread" style="background-image: url('<?=base_url();?>images/dvu8.png');">
+      <div class="container">
+        <div class="row no-gutters slider-text align-items-center justify-content-center">
+          <div class="col-md-9 ftco-animate text-center">
+            
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="breadcrumb-area">
+        <!-- Top Breadcrumb Area -->
+
+    <div class="hero-wrap hero-bread" style="background-image: url('images/bg_1.jpg');">
+      <div class="container">
+        <div class="row no-gutters slider-text align-items-center justify-content-center">
+          <div class="col-md-9 ftco-animate text-center">
+          	<p class="breadcrumbs"><span class="mr-2"><a href="index.html">Trang chủ></a></span> <span>Dịch vụ</span></p>
+            <h1 class="mb-0 bread">Dịch vụ cắt tỉa lông chó mèo</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+<div class="col-md-6 d-flex align-items-center">
+            
+          </div>
+        </div>
+      </div>
+    <section class="ftco-section">
+    	<div class="container">
+    		
+    		<h2>DỊCH VỤ SPA CẮT TỈA LÔNG CHÓ MÈO</h2>
+            <h6>⚡⚡⚡BẠN ĐANG TÌM KIẾM ĐỊA CHỈ CẮT TỈA LÔNG CHÓ MÈO CHUYÊN NGHIỆP Ở TPHCM ??? ⚡⚡
+👉👉 TẠI ARALE PETSHOP,CHÚNG TÔI CUNG CẤP TẤT CẢ CÁC LOẠI DỊCH VỤ CHĂM SÓC VÀ LÀM ĐẸP TRỌN GÓI VỚI GIÁ CẢ VÔ CÙNG HỢP LÍ CHO THÚ CƯNG NHÀ BẠN.</h6>
+            <p>❌ Cắt tỉa lông chó mèo vô cùng quan trọng,việc cắt tỉa lông cho chó mèo không những giúp cho thú cưng đảm bảo về sức khỏe mà còn hạn chế nhiều vấn đề như : ve rận,bọ chét,kí sinh trùng và còn tránh được tình trạng viêm da.
+♦️Việc sử dụng dịch vụ cắt tỉa lông tại ARALE PETSHOP định kì và thường xuyên sẽ mang lại nhiều lợi ích cho thú cưng nhà bạn đấy .
+Hãy dành thời gian đưa Pet cưng của mình đến với dịch vụ của chúng tôi.Với đội ngũ nhân viên được đào tạo nhiều năm chuyên nghiệp,chắc chắc chúng tôi sẽ đáp ứng hết tất cả các nhu cầu làm đẹp cho tất cả các thú cưng của bạn.
+Dịch vụ cắt tỈa lông chó mèo của chúng tôi luôn được sử dụng những dòng sản phẩm sữa tắm tốt nhất đảm bảo an toàn cho pet cưng nhà bạn.</p>
+            <p>🍀 DỊCH VỤ CẮT TỈA LÔNG - GROOMING CHÓ MÈO CỦA ARALE PETSHOP GỒM 10 BƯỚC : 
+            <ul>
+<li>1. Vệ sinh - nhổ lông tai, khử mùi hôi</li>
+<li>2. Cắt – dũa móng</li>
+<li>3. Cạo vệ sinh bàn chân, lông bụng, hậu môn</li>
+<li>4. Gỡ lông rối</li>
+<li>5. Tắm gội bằng nước ấm</li>
+<li>6. Vắt tuyến hôi</li>
+<li>7. Massge</li>
+<li>8. Sấy đánh tơi </li>
+<li>9. Cắt tạo kiểu </li>
+<li>10. Xịt dưỡng lông, xịt nước hoa</li>
+</ul>
+</p>
+			<a class="img-prod"><img class="img-fluid" src="images/bang_gia.jpg" alt="Colorlib Template" style="width: 900px; height: 950px">
+            <p>Đặc biệt hơn,bạn sẽ được nhân viên của chúng tôi tư vấn làm thế nào để giúp cho thú cưng giữ kiểu lông lâu hơn và chăm sóc da và lông được tốt hơn.</p>
+<p>📞📞 🍀Book lịch để làm đẹp cho thú cưng của nhà bạn ngay: 0979 1646 76</p>
+
+---------------------------------------------------------------------
+
+<p>ARALE PETSHOP luôn có chương trình ưu đãi tích lũy điểm dành cho khách hàng thân thiết .Đặc biệt freeship cho đơn hàng đặt vào thứ 3 hàng tuần và đơn hàng trên 1 triệu
+💥💥💥 Miễn phí giao hàng quận Gò Vấp , 5 , 6 , 10 và 11</p>
+
+<p>🏠 731 TÂN SƠN, PHƯỜNG 12, QUẬN GÒ VẤP<br>
+☎️ 028 22531199<br>
+
+🏠 604 PHAN VĂN TRỊ, PHƯỜNG 7, QUẬN GÒ VẤP<br>
+☎️ 0979 16 46 76<br>
+
+🏠 213D MINH PHỤNG,PHƯỜNG 9, QUẬN 6<br>
+☎️ 028 22536658<br>
+
+🛒 Mua sỉ/Hotline: 0979 16 46 76 [ MS. THẢO]<br>
+🌍 Fanpage: https://www.facebook.com/AralePetshop/
+🌐 : www.aralepetshop.vn<br>
+
+🚀Shoppe: https://shopee.vn/aralepetshop<br>
+
+08/08/2019</p>
+    			
+    		<div class="row mt-5">
+          <div class="col text-center">
+            <div class="block-27">
+              <ul>
+                <li><a href="#">&lt;</a></li>
+                <li class="active"><span>1</span></li>
+                <li><a href="#">2</a></li>
+                <li><a href="#">3</a></li>
+                <li><a href="#">4</a></li>
+                <li><a href="#">5</a></li>
+                <li><a href="#">&gt;</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+    	</div>
+
+    </section>
+
+	<section class="ftco-section ftco-no-pt ftco-no-pb py-5 bg-light">
+      <div class="container py-4">
+        <div class="row d-flex justify-content-center py-5">
+          
+          <div class="col-md-6 d-flex align-items-center">
+            <div id="booking" class="section">
+		<div class="section-center">
+			<div class="container">
+				<div class="row">
+					<div class="booking-form">
+						<div class="booking-bg">
+							<div class="form-header">
+								<h2>Đặt lịch online giảm ngay 10%</h2>
+								<h6>VUI LÒNG ĐỂ LẠI THÔNG TIN ĐẦY ĐỦ SẼ GIÚP BOOKCAREPET <br>PHỤC VỤ BẠN TỐT NHẤT</h6>
+							</div>
+						</div>
+						<form>
+							<div class="row">
+								<div class="col-md-6">
+									<div class="form-group">
+										<span class="form-label">Họ tên* </span>
+										<td>
+                      						<div class="input-group mb-3">
+                        						<input type="text" required>
+                      						</div>
+                   						</td>
+										<span class="select-arrow"></span>
+									</div>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-md-6">
+									<div class="form-group">
+										<span class="form-label" >Điện thoại* </span>
+										<td>
+                      						<div class="input-group mb-3">
+                        						<input type="text" required>
+                      						</div>
+                   						</td>
+										<span class="select-arrow"></span>
+									</div>
+								</div>
+							</div>
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <span class="form-label" >Email* </span>
+                    <td>
+                                  <div class="input-group mb-3">
+                                    <input type="text" required>
+                                  </div>
+                              </td>
+                    <span class="select-arrow"></span>
+                  </div>
+                </div>
+              </div>
+							<div class="row">
+								<div class="col-md-6">
+									<div class="form-group">
+										<span class="form-label" >Địa chỉ* </span>
+										<td>
+                      						<div class="input-group mb-3">
+                        						<input type="text" required>
+                      						</div>
+                   						</td>
+										<span class="select-arrow"></span>
+									</div>
+								</div>
+							</div>
+							<div class="form-group">
+								<span class="form-label">Giống thú cưng</span>
+								<select class="form-control" required>
+									<option value="" selected hidden>Chọn</option>
+									<option>Chó</option>
+									<option>Mèo</option>
+									<option>Khác</option>
+								</select>
+								<span class="select-arrow"></span>
+							</div>
+							<div class="form-group">
+								<span class="form-label">Dịch vụ</span>
+								<select class="form-control" required>
+									<option value="" selected hidden>Chọn dịch vụ</option>
+									<option>Dịch vụ cung cấp đồ cho thú cưng</option>
+									<option>Dịch vụ chăm sóc- khám chữa thú cưng</option>
+								</select>
+								<span class="select-arrow"></span>
+							</div>
+							<div class="row">
+								<div class="col-md-6">
+									<div class="form-group">
+										<span class="form-label">Ngày thực hiện khám</span>
+										<input class="form-control" type="date" required>
+									</div>
+								</div>
+								
+							</div>
+							
+							<div class="form-group">
+								<span class="form-label">Phòng khám</span>
+								<select class="form-control" required>
+									<option value="" selected hidden>Chọn phòng khám</option>
+									<option>Phòng khám Số 3 Văn Cao</option>
+									<option>Phòng khám Ngõ 25 Phố Nguyễn Cơ Thạch</option>
+								</select>
+								<span class="select-arrow"></span>
+							</div>
+							
+							<div class="form-btn">
+								<button class="submit-btn">ĐẶT</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+          </div>
+        </div>
+      </div>
+    </section>
+		
+    <footer class="ftco-footer ftco-section">
+      <div class="container">
+      	<div class="row">
+      		<div class="mouse">
+						<a href="#" class="mouse-icon">
+							<div class="mouse-wheel"><span class="ion-ios-arrow-up"></span></div>
+						</a>
+					</div>
+      	</div>
+        <div class="row mb-5">
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">BookCarePets</h2>
+              <p>Trung gian kết nối giữa chủ thú cưng với bác sĩ, phòng khám, cơ sở thú y</p>
+              <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
+                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4 ml-md-5">
+              <h2 class="ftco-heading-2">Menu</h2>
+              <ul class="list-unstyled">
+                <li><a href="#" class="py-2 d-block">Dịch vụ</a></li>
+                <li><a href="#" class="py-2 d-block">Tin tức</a></li>
+                <li><a href="#" class="py-2 d-block">Liên hệ</a></li>
+               
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-4">
+             <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">Giúp đỡ</h2>
+              <div class="d-flex">
+	              <ul class="list-unstyled mr-l-5 pr-l-3 mr-4">
+	                <li><a href="#" class="py-2 d-block">Thông tin địa chỉ phòng khám</a></li>
+	                <li><a href="#" class="py-2 d-block">Thông tin bác sỹ</a></li>
+	                <li><a href="#" class="py-2 d-block">Thông tin nơi cung cấp đồ cho thú cưng</a></li>
+	                <li><a href="#" class="py-2 d-block">Thông tin giá cả đồ cho thú cưng</a></li>
+	              </ul>
+	              
+	            </div>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+            	<h2 class="ftco-heading-2">Bạn đang có câu hỏi?</h2>
+            	<div class="block-23 mb-3">
+	              <ul>
+	                <li><span class="icon icon-map-marker"></span><span class="text">12 Chùa Bộc, Hà Nội, Việt Nam</span></li>
+	                <li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929 210</span></a></li>
+	                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">bookcarepet.com</span></a></li>
+	              </ul>
+	            </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 text-center">
+
+            
+          </div>
+        </div>
+      </div>
+    </footer>
+    
+  
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/bootstrap-datepicker.js"></script>
+  <script src="js/scrollax.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="js/google-map.js"></script>
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
